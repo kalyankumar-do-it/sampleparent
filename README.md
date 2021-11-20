4d1f2bde1ca63c533a56a89ae870cbbf5e466520
kalyankumar-do-it <78216127+kalyankumar-do-it@users.noreply.github.com>
2021-11-20 18:19:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# sampleparent
+ sampleparent
---
+++ core/src/main/java/core/com/techjs/SampleCore.java
@@ -0,0 +1,11 @@
+package core.com.techjs;
+
+public class SampleCore {
+
+	public static void main(String[] args) 
+	{
+	  System.out.println("Hi");
+
+	}
+
+}
